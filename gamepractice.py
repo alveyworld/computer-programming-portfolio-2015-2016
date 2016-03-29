cbb9ec9d07adb53a081378c42e4393bf1401fc84
Zack Alvey <zackalvey@Alveyworld-00.local>
2016-03-29 10:44:50 -0600

    second try

--- gamepractice.py
+++ gamepractice.py
@@ -4,27 +4,40 @@ import sys
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Drawing time')
 
-BLACK = (  0,   0,   0) # R G B
+DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
+pygame.display.set_caption("Drawing stuff")
+
+#Palette of colors
+BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
 RED   = (255,   0,   0)
 GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
+RANDOM= (210, 254,  17)
 
-DISPLAYSURF.fill(WHITE) 
-pygame.draw.rect(DISPLAYSURF, RED, (0, 0, 100, 100))
-#draw 100x100 BLUE rectangle in the top right corner
-pygame.draw.rect(DISPLAYSURF, GREEN, (300, 200, 400, 300))
-pygame.draw.line(DISPLAYSURF, BLUE, (0, 0), (400, 300), 4)
-pygame.draw.circle(DISPLAYSURF, BLACK, (200, 150), 50, 0)
-pygame.draw.polygon(DISPLAYSURF, GREEN, ((10,10), (100, 10), (150, 30), (200, 50), (20, 200)))
-pygame.draw.ellipse(DISPLAYSURF, RED, (10, 10, 50, 30))
+DISPLAYSURF.fill(RANDOM)
+pygame.draw.line(DISPLAYSURF, WHITE, (100, 28), (300, 200), 4)
+pygame.draw.line(DISPLAYSURF, BLACK, (0,0), (400, 300), 4)
+pygame.draw.line(DISPLAYSURF, BLACK, (0,300), (400,0), 4)
+pygame.draw.polygon(DISPLAYSURF, RED, ((200,0),(250, 50),(225,100),(175,100),(150,50)))
+pygame.draw.circle(DISPLAYSURF, BLUE, (275, 140), 48, 0)
+pygame.draw.rect(DISPLAYSURF, BLACK, (0, 0, 200, 100), 0)
+pygame.draw.ellipse(DISPLAYSURF, GREEN, (0, 0, 200, 100), 0)
 
-while True: # main game loop
+#draw a blue 120x100 rectangle in the bottom right corner 
+
+#draw a black hexagon each side 25 anywhere
+
+#OCTAGON
+pygame.draw.polygon(DISPLAYSURF, BLACK, ((200,100),(225,100),(242.67,117.67),(242.67,142.67),(225,160.34),(200,160.34),(182.33,142.67),(182.33,117.67)),5)
+pygame.draw.polygon(DISPLAYSURF, WHITE, ((200,100),(225,100),(242.67,117.67),(242.67,142.67),(225,160.34),(200,160.34),(182.33,142.67),(182.33,117.67)))
+
+
+while True:
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
 	pygame.display.update()
+	
